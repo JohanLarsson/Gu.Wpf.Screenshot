e0ffa2b8475576e3ca8720d43f231284ff7892a5
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-27 01:08:03 +0200

    Initial

---
+++ Gu.Wpf.Screenshot/ScreenCapture.Commands.cs
@@ -0,0 +1,44 @@
+namespace Gu.Wpf.Screenshot
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Input;
+
+    public static partial class ScreenCapture
+    {
+        public static ICommand ShowSnippingToolCommand { get; } = _ShowSippingToolCommand.Default;
+
+        private class _ShowSippingToolCommand : ICommand
+        {
+            public static readonly ICommand Default = new _ShowSippingToolCommand();
+
+            private readonly ProcessStartInfo startInfo;
+
+            private _ShowSippingToolCommand()
+            {
+                var fileName = Environment.Is64BitProcess
+                    ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "SnippingTool.exe")
+                    : @"C:\Windows\sysnative\SnippingTool.exe";
+
+                if (File.Exists(fileName))
+                {
+                    this.startInfo = new ProcessStartInfo(fileName);
+                }
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter) => this.startInfo != null;
+
+            public void Execute(object parameter)
+            {
+                if (startInfo != null)
+                {
+                    Process.Start(startInfo);
+                }
+            }
+        }
+    }
+}
